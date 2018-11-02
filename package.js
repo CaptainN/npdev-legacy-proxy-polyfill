9f9e59b290f9d0d69f44ba6296c0819ab4ca88e8
Kevin Newman <Kevin@unFocus.com>
2018-11-01 23:37:47 -0400

    use this package's git location, instead of the npm package's git repo in package.js

--- package.js
+++ package.js
@@ -3,7 +3,7 @@ Package.describe({
   name: 'npdev:legacy-proxy-polyfill',
   summary: 'Polyfill for the Proxy object from the Google Chrome team',
   version: '0.2.0',
-  git: 'https://github.com/GoogleChrome/proxy-polyfill.git'
+  git: 'https://github.com/CaptainN/npdev-legacy-proxy-polyfill'
 })
 
 Npm.depends({ 'proxy-polyfill': '0.2.0' })
